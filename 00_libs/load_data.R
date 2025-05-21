96cde8cff9a062326ff783c6e8bdd82779cbad27
lpantano <lpantano@users.noreply.github.com>
2025-05-21 20:04:24 +0000

    Style code (GHA)

--- 00_libs/load_data.R
+++ 00_libs/load_data.R
@@ -6,132 +6,130 @@ load_metrics <- function(se = se_object, multiqc = multiqc_data_dir,
                          gtf = gtf_fn,
                          counts = counts,
                          single_end = FALSE) {
+  # Get metrics from nf-core into bcbio like table
+  # many metrics are already in the General Table of MultiQC, this reads the file
+  metrics <- read_tsv(file.path(multiqc_data_dir, "multiqc_general_stats.txt"))
 
+  # we use the names in the multiqc general stats file to determine which version of the pipeline was used.
+  # this affects other metrics processing throughout this function.
+  if (any(grepl("mqc-generalstats", names(metrics)))) {
+    version <- "3.14"
+  } else {
+    version <- "3.18"
+  }
 
-    # Get metrics from nf-core into bcbio like table
-    # many metrics are already in the General Table of MultiQC, this reads the file
-    metrics <- read_tsv(file.path(multiqc_data_dir, "multiqc_general_stats.txt"))
+  # we get some more metrics from Qualimap and rename columns
+  if (version == 3.14) {
+    metrics_qualimap <- read_tsv(file.path(multiqc_data_dir, "mqc_qualimap_genomic_origin_1.txt"))
+  } else {
+    metrics_qualimap <- read_tsv(file.path(multiqc_data_dir, "qualimap_genomic_origin.txt"))
+  }
 
-    # we use the names in the multiqc general stats file to determine which version of the pipeline was used.
-    # this affects other metrics processing throughout this function.
-    if (any(grepl("mqc-generalstats", names(metrics)))) {
-      version <- "3.14"
-    } else {
-      version <- "3.18"
-    }
+  metrics <- metrics %>% full_join(metrics_qualimap)
+  metrics <- metrics %>%
+    clean_names()
 
-    # we get some more metrics from Qualimap and rename columns
-    if (version == 3.14) {
-      metrics_qualimap <- read_tsv(file.path(multiqc_data_dir, "mqc_qualimap_genomic_origin_1.txt"))
-    } else {
-      metrics_qualimap <- read_tsv(file.path(multiqc_data_dir, "qualimap_genomic_origin.txt"))
-    }
+  if (version == "3.14") {
+    metrics <- metrics %>% dplyr::rename_with(~ gsub(".*mqc_generalstats_", "", .))
+  }
 
-    metrics <- metrics %>% full_join(metrics_qualimap)
+  # This uses the fastqc metrics to get total reads
+  total_reads <- metrics %>%
+    dplyr::filter(!is.na(fastqc_raw_total_sequences)) %>%
+    remove_empty(which = "cols") %>%
+    dplyr::rename(single_sample = sample) %>%
+    mutate(sample = gsub("_[12]+$", "", single_sample)) %>%
+    group_by(sample) %>%
+    summarize(total_reads = sum(fastqc_raw_total_sequences))
+
+  # This renames to user-friendly names the metrics columns
+  if (single_end) {
     metrics <- metrics %>%
-      clean_names()
-
-    if (version == "3.14") {
-      metrics <- metrics %>% dplyr::rename_with(~ gsub(".*mqc_generalstats_", "", .))
-    }
-
-    # This uses the fastqc metrics to get total reads
-    total_reads <- metrics %>%
-      dplyr::filter(!is.na(fastqc_raw_total_sequences)) %>%
-      remove_empty(which = "cols") %>%
-      dplyr::rename(single_sample = sample) %>%
-      mutate(sample = gsub("_[12]+$", "", single_sample)) %>%
-      group_by(sample) %>%
-      summarize(total_reads = sum(fastqc_raw_total_sequences))
-
-    # This renames to user-friendly names the metrics columns
-    if (single_end) {
-      metrics <- metrics %>%
-        dplyr::filter(!is.na(fastqc_raw_total_sequences))
-    } else {
-      metrics <- metrics %>%
-        dplyr::filter(is.na(fastqc_raw_total_sequences))
-    }
-
+      dplyr::filter(!is.na(fastqc_raw_total_sequences))
+  } else {
     metrics <- metrics %>%
-      remove_empty(which = "cols") %>%
-      full_join(total_reads)
+      dplyr::filter(is.na(fastqc_raw_total_sequences))
+  }
 
-    if (version == "3.14") {
-      metrics <- metrics %>% mutate(mapped_reads = samtools_reads_mapped)
-    } else {
-      metrics <- metrics %>% mutate(mapped_reads = samtools_stats_reads_mapped)
+  metrics <- metrics %>%
+    remove_empty(which = "cols") %>%
+    full_join(total_reads)
+
+  if (version == "3.14") {
+    metrics <- metrics %>% mutate(mapped_reads = samtools_reads_mapped)
+  } else {
+    metrics <- metrics %>% mutate(mapped_reads = samtools_stats_reads_mapped)
+  }
+
+  metrics <- metrics %>%
+    rowwise() %>%
+    mutate(exonic_rate = exonic / (exonic + intronic + intergenic)) %>%
+    mutate(intronic_rate = intronic / (exonic + intronic + intergenic)) %>%
+    mutate(intergenic_rate = intergenic / (exonic + intronic + intergenic))
+
+  if (version == "3.14") {
+    metrics <- metrics %>% mutate(x5_3_bias = qualimap_5_3_bias)
+  } else {
+    metrics <- metrics %>% mutate(x5_3_bias = qualimap_rnaseq_5_3_bias)
+  }
+
+  # Sometimes we don't have rRNA due to mismatch annotation, We skip this if is the case
+  gtf <- NULL
+  biotype <- NULL
+
+  if (genome == "other") {
+    gtf <- gtf_fn
+  } else {
+    if (genome == "hg38") {
+      gtf <- "hg38.rna.gtf.gz"
+    } else if (genome == "mm10") {
+      gtf <- "mm10.rna.gtf.gz"
+    } else if (genome == "mm39") {
+      gtf <- "mm39.rna.gtf.gz"
     }
+    gtf <- file.path("https://github.com/bcbio/bcbioR/raw/refs/heads/main/inst/extdata/annotation", gtf)
+  }
+  if (is.null(gtf)) {
+    warning("No genome provided! Please add it at the top of this Rmd")
+  } else {
+    gtf <- rtracklayer::import(gtf)
 
-    metrics <- metrics %>%
-      rowwise() %>%
-      mutate(exonic_rate = exonic / (exonic + intronic + intergenic)) %>%
-      mutate(intronic_rate = intronic / (exonic + intronic + intergenic)) %>%
-      mutate(intergenic_rate = intergenic / (exonic + intronic + intergenic))
-
-    if (version == "3.14") {
-      metrics <- metrics %>% mutate(x5_3_bias = qualimap_5_3_bias)
+    one <- grep("gene_type", colnames(as.data.frame(gtf)), value = TRUE)
+    another <- grep("gene_biotype", colnames(as.data.frame(gtf)), value = TRUE)
+    if (length(one) == 1) {
+      biotype <- one
+    } else if (length(another) == 1) {
+      biotype <- another
     } else {
-      metrics <- metrics %>% mutate(x5_3_bias = qualimap_rnaseq_5_3_bias)
+      warning("No gene biotype founded")
     }
+  }
 
-    # Sometimes we don't have rRNA due to mismatch annotation, We skip this if is the case
-    gtf <- NULL
-    biotype <- NULL
+  metrics$sample <- make.names(metrics$sample)
+  if (!is.null(biotype)) {
+    annotation <- as.data.frame(gtf) %>% .[, c("gene_id", biotype)]
+    rRNA <- grepl("rRNA|tRNA", annotation[[biotype]])
+    genes <- intersect(annotation[rRNA, "gene_id"], row.names(counts))
+    ratio <- data.frame(
+      sample = colnames(counts),
+      r_and_t_rna_rate = colSums(counts[genes, ]) / colSums(counts)
+    )
+    metrics <- left_join(metrics, ratio, by = "sample")
+  } else {
+    metrics[["r_and_t_rna_rate"]] <- NA
+  }
 
-    if (genome == "other") {
-      gtf <- gtf_fn
-    } else {
-      if (genome == "hg38") {
-        gtf <- "hg38.rna.gtf.gz"
-      } else if (genome == "mm10") {
-        gtf <- "mm10.rna.gtf.gz"
-      } else if (genome == "mm39") {
-        gtf <- "mm39.rna.gtf.gz"
-      }
-      gtf <- file.path("https://github.com/bcbio/bcbioR/raw/refs/heads/main/inst/extdata/annotation", gtf)
-    }
-    if (is.null(gtf)) {
-      warning("No genome provided! Please add it at the top of this Rmd")
-    } else {
-      gtf <- rtracklayer::import(gtf)
+  # if ("custom_content_biotype_counts_percent_r_rna" %in% colnames(metrics)){
+  #   metrics <- mutate(metrics, r_rna_rate = custom_content_biotype_counts_percent_r_rna)
+  # }else{
+  #  metrics[["r_rna_rate"]] <- NA
+  # }
+  metrics <- metrics[, c(
+    "sample", "mapped_reads", "exonic_rate", "intronic_rate",
+    "total_reads",
+    "x5_3_bias", "r_and_t_rna_rate", "intergenic_rate"
+  )]
 
-      one <- grep("gene_type", colnames(as.data.frame(gtf)), value = TRUE)
-      another <- grep("gene_biotype", colnames(as.data.frame(gtf)), value = TRUE)
-      if (length(one) == 1) {
-        biotype <- one
-      } else if (length(another) == 1) {
-        biotype <- another
-      } else {
-        warning("No gene biotype founded")
-      }
-    }
-
-    metrics$sample <- make.names(metrics$sample)
-    if (!is.null(biotype)) {
-      annotation <- as.data.frame(gtf) %>% .[, c("gene_id", biotype)]
-      rRNA <- grepl("rRNA|tRNA", annotation[[biotype]])
-      genes <- intersect(annotation[rRNA, "gene_id"], row.names(counts))
-      ratio <- data.frame(
-        sample = colnames(counts),
-        r_and_t_rna_rate = colSums(counts[genes, ]) / colSums(counts)
-      )
-      metrics <- left_join(metrics, ratio, by = "sample")
-    } else {
-      metrics[["r_and_t_rna_rate"]] <- NA
-    }
-
-    # if ("custom_content_biotype_counts_percent_r_rna" %in% colnames(metrics)){
-    #   metrics <- mutate(metrics, r_rna_rate = custom_content_biotype_counts_percent_r_rna)
-    # }else{
-    #  metrics[["r_rna_rate"]] <- NA
-    # }
-    metrics <- metrics[, c(
-      "sample", "mapped_reads", "exonic_rate", "intronic_rate",
-      "total_reads",
-      "x5_3_bias", "r_and_t_rna_rate", "intergenic_rate"
-    )]
-  
   rownames(metrics) <- metrics$sample
   return(metrics)
 }
