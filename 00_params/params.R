3e78388b5eff73ad331f60be1ef252c82ce5aea2
lpantano <lpantano@users.noreply.github.com>
2025-06-17 19:03:48 +0000

    Style code (GHA)

--- 00_params/params.R
+++ 00_params/params.R
@@ -8,7 +8,7 @@ coldata_fn <- "/Path/to/metadata/meta.csv"
 # This file is inside star_salmon/ folder
 # Use gene_counts_length_scaled.tsv to use the transcript-level abundance estimates to calculate a gene-level offset that corrects for changes to the average transcript length across samples
 #   See more here: https://bioconductor.org/packages/devel/bioc/vignettes/tximport/inst/doc/tximport.html#Downstream_DGE_in_Bioconductor
-#   This is correct because in nf-core/rnaseq this files is created with tximport argument countsFromAbundance="lengthScaledTPM" 
+#   This is correct because in nf-core/rnaseq this files is created with tximport argument countsFromAbundance="lengthScaledTPM"
 #   https://github.com/nf-core/rnaseq/blob/0bb032c1e3b1e1ff0b0a72192b9118fdb5062489/modules/nf-core/tximeta/tximport/templates/tximport.r#L152
 counts_fn <- "/path/to/nf-core/output/star_salmon/salmon.merged.gene_counts.tsv"
 # This folder called "multiqc_report_data" is inside the output directory star_salmon inside multiqc folder
