a4558fc47a28bc1cd1405ad96bc437b53560187d
Lorena Pantano <lorena.pantano@gmail.com>
2025-07-02 17:46:15 -0400

    add snapshot of qc

--- install_dependencies.R
+++ install_dependencies.R
@@ -1,3 +1,6 @@
 install.packages("BiocManager")
 BiocManager::install("renv")
 BiocManager::install(renv::dependencies(path = ".")[["Package"]])
+
+renv::snapshot(lockfile = "01_quality_assessment/renv.lock",
+               packages = renv::dependencies(path = "01_quality_assessment")[["Package"]])
