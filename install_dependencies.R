236076b6d7f03669427ee9c1a9edef8bd6da3d91
Lorena Pantano <lorena.pantano@gmail.com>
2025-07-02 22:12:03 -0400

    update deps in renv

--- install_dependencies.R
+++ install_dependencies.R
@@ -3,11 +3,15 @@ BiocManager::install("renv")
 BiocManager::install("omnideconv/immunedeconv")
 BiocManager::install(renv::dependencies(path = ".")[["Package"]])
 
+pkgs <- c(renv::dependencies(path = "01_quality_assessment")[["Package"]],
+          renv::dependencies(path = "00_libs")[["Package"]])
 renv::snapshot(lockfile = "01_quality_assessment/renv.lock",
-               packages = renv::dependencies(path = "01_quality_assessment")[["Package"]])
+               packages = pkgs)
 
+pkgs <- c(renv::dependencies(path = "02_differential_expression")[["Package"]],
+          renv::dependencies(path = "00_libs")[["Package"]])
 renv::snapshot(lockfile = "02_differential_expression/renv.lock",
-               packages = renv::dependencies(path = "02_differential_expression")[["Package"]])
+               packages = pkgs)
 
 renv::snapshot(lockfile = "03_comparative/renv.lock",
                packages = renv::dependencies(path = "03_comparative")[["Package"]])
